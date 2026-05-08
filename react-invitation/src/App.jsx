136df4215b60bbd8ad6b6cbc96c28594c434ab96
Hassanikhadidja <Khadidja.khaoila.hassani@gmail.com>
2026-05-08 18:59:18 +0100

    first commit

---
+++ react-invitation/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app-shell">
+      <iframe
+        className="invitation-frame"
+        src="/wedding_invitation.html"
+        title="Wedding Invitation"
+      />
+    </main>
+  )
+}
+
+export default App
